467353fd1482e4344cebd40e8b63b31024a29956
unohayato <mamidori1p1p@gmail.com>
2022-12-03 15:03:17 +0900

    settings

--- chat/views.py
+++ chat/views.py
@@ -5,5 +5,7 @@ from django.shortcuts import render
 def index(request):
   return render(request, 'chat/index.html', {})
 
-def room(request):
-  return render(request, 'chat/chatroom.html', {})
+def room(request, room_name):
+  return render(request, 'chat/chatroom.html', {
+    'room_name': room_name,
+  })
