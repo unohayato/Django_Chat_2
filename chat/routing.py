763a42330c6e04f3b380b5cb35c2d3c5a2d0b518
unohayato <mamidori1p1p@gmail.com>
2022-12-03 15:32:58 +0900

    consumer and chat.html

--- chat/routing.py
+++ chat/routing.py
@@ -1,5 +1,7 @@
 from django.urls import re_path
 
+from . import consumers
+
 websocket_urlpatterns = [
-  re_path(r'ws/chat/(?P<room_name>\w+)', Consumers.ChatRoomConsumer),
+  re_path(r'ws/chat/(?P<room_name>\w+)', consumers.ChatRoomConsumer),
 ]
